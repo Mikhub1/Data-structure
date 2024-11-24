f1bcf749104fef3df4285b7ed5262ec69aae0d62
Mikhub1 <122497460+Mikhub1@users.noreply.github.com>
2024-11-24 02:58:53 -0330

    Delete Data_structure_python/r4.py

--- Data_structure_python/r4.py
+++
@@ -1,9 +0,0 @@
-recursive_search(self, head, x):
-        if head is None:
-            return False
-        if head.data == x:
-            return True
-        return self.recursive_search(head.next, x)
-
-    def search(self, x):
-        return self.recursive_search(self.head, x)
